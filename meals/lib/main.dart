70fd7be1aafea8755cc94a55e557ea484e2fb82a
Thyago Henrique <thyagobs.al@gmail.com>
2024-02-16 05:15:07 -0300

    commit

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/categories_screen.dart';
+import 'screens/tabs_screen.dart';
+// import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       routes: {
-        AppRoutes.home: (ctx) => const CategoriesScreen(),
+        AppRoutes.home: (ctx) => const TabsScreen(),
         AppRoutes.categoriesMeals: (ctx) => const CategoriesMealsScreen(),
         AppRoutes.mealDetail: (ctx) => const MealDetailScreen(),
       },
